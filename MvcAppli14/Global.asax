4b27a4753759fd8a1d2024e19e2cb82cebd4aa30
Rishi <suryawanshirishi.nmims@gmail.com>
2020-08-01 17:05:17 +0530

    MvcAppli14

---
+++ MvcAppli14/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcAppli14.MvcApplication" Language="C#" %>
---
+++ MvcAppli14/Models/Empl1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using MvcAppli14.Common;
+
+namespace MvcAppli14.Models
+{
+    [MetadataType(typeof(EmplMetaData))]
+    public partial class Empl
+    {
+    }
+
+    public class EmplMetaData
+    {
+        [RemoteClientServer("IsUserNameAvailable", "Home", 
+        ErrorMessage="UserName already in use")]
+        public string FirstName { get; set; }
+    }
+}
